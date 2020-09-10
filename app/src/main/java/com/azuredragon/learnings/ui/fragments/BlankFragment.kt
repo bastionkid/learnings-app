171ba8ddcc636389ffb2446e6d95036e995c5423
bastionkid <akash.mercer@gmail.com>
2020-09-10 10:24:22 +0530

    1) cute_cat_works.json : Lottie json added

--- app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
@@ -1,38 +1,13 @@
 package com.azuredragon.learnings.ui.fragments
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.azuredragon.learnings.R
 import com.azuredragon.learnings.databinding.FragmentBlankBinding
 import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
-import com.azuredragon.learnings.performance.logJvmHeapMemoryInfo
-import com.azuredragon.learnings.performance.logNativeHeapMemoryInfo
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class BlankFragment : Fragment(R.layout.fragment_blank) {
 
     private val blankBinding: FragmentBlankBinding by dataBindingsLazy()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        lifecycleScope.launch {
-            delay(10000)
-
-            logJvmHeapMemoryInfo("After 10s of onViewCreated() of ${BlankFragment::class.java.canonicalName}")
-            logNativeHeapMemoryInfo("After 10s of onViewCreated() of ${BlankFragment::class.java.canonicalName}")
-
-            System.gc()
-
-            delay(10000)
-
-            logJvmHeapMemoryInfo("After 10s of running manual GC of ${BlankFragment::class.java.canonicalName}")
-            logNativeHeapMemoryInfo("After 10s of running manual GC of ${BlankFragment::class.java.canonicalName}")
-        }
-    }
 
     companion object {
         @JvmStatic
--- app/src/main/java/com/azuredragon/learnings/ui/fragments/LottieFragment.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/LottieFragment.kt
@@ -3,16 +3,10 @@ package com.azuredragon.learnings.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.azuredragon.learnings.R
 import com.azuredragon.learnings.databinding.FragmentLottieBinding
 import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
-import com.azuredragon.learnings.ktxextensions.gone
 import com.azuredragon.learnings.ktxextensions.navigateSafely
-import com.azuredragon.learnings.performance.getCpuStats
-import com.azuredragon.learnings.performance.logJvmHeapMemoryInfo
-import com.azuredragon.learnings.performance.logNativeHeapMemoryInfo
-import kotlinx.coroutines.delay
 
 class LottieFragment : Fragment(R.layout.fragment_lottie) {
 
@@ -25,24 +19,6 @@ class LottieFragment : Fragment(R.layout.fragment_lottie) {
             setOnNextClickListener {
                 navigateSafely(R.id.action_lottieFragment_to_constraintLayoutLayerFragment)
             }
-        }
-
-        lifecycleScope.launchWhenResumed {
-            delay(10000)
-
-            logJvmHeapMemoryInfo("When Visibility = View.VISIBLE")
-            logNativeHeapMemoryInfo("When Visibility = View.VISIBLE")
-
-            delay(10000)
-
-            lottieBinding.lav.gone()
-
-            delay(10000)
-
-            logJvmHeapMemoryInfo("After Visibility = View.GONE")
-            logNativeHeapMemoryInfo("After Visibility = View.GONE")
-
-            navigateSafely(R.id.action_lottieFragment_to_blankFragment)
         }
     }
 
