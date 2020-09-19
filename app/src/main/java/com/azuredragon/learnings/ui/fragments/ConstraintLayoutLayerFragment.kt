0e34b083529c94ad4b8cecb595beb2f5fd0870cf
bastionkid <akash.mercer@gmail.com>
2020-09-19 16:48:29 +0530

    1) build.gradle : dependencies version bumped

--- app/src/main/java/com/azuredragon/learnings/ui/fragments/ConstraintLayoutLayerFragment.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/ConstraintLayoutLayerFragment.kt
@@ -1,15 +1,12 @@
 package com.azuredragon.learnings.ui.fragments
 
 import android.os.Bundle
-import android.transition.TransitionManager
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.azuredragon.learnings.R
 import com.azuredragon.learnings.databinding.FragmentConstraintLayoutLayerBinding
 import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
-import kotlinx.coroutines.delay
 
 class ConstraintLayoutLayerFragment : Fragment(R.layout.fragment_constraint_layout_layer) {
 
@@ -19,10 +16,7 @@ class ConstraintLayoutLayerFragment : Fragment(R.layout.fragment_constraint_layo
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launchWhenResumed {
-            delay(10000)
-
-            TransitionManager.beginDelayedTransition(constraintLayoutLayerBinding.root as ViewGroup)
-            constraintLayoutLayerBinding.layerButtons.rotation = -45f
+            constraintLayoutLayerBinding.layerButtons.scaleX = 1.5f
         }
     }
 
