71a2fbe79b547aec12923e7f6a4234f97f4e225f
bastionkid <akash.mercer@gmail.com>
2020-10-19 08:40:33 +0530

    1) ExceptionThrowAndCatchFragment.kt : added

---
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/ExceptionThrowAndCatchFragment.kt
@@ -0,0 +1,43 @@
+package com.azuredragon.learnings.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.azuredragon.learnings.R
+import com.azuredragon.learnings.databinding.FragmentExceptionThrowAndCatchBinding
+import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
+import com.azuredragon.learnings.ui.activities.MainActivity
+import com.azuredragon.learnings.ui.utils.UncaughtExceptionHandler
+import kotlinx.coroutines.delay
+
+class ExceptionThrowAndCatchFragment : Fragment(R.layout.fragment_exception_throw_and_catch) {
+
+    private val exceptionThrowAndCatchBinding: FragmentExceptionThrowAndCatchBinding by dataBindingsLazy()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        exceptionThrowAndCatchBinding.exceptionText = getString(R.string.label_throwing_exception)
+
+        lifecycleScope.launchWhenResumed {
+            delay(5000)
+
+            throw Exception()
+        }
+
+        Thread.setDefaultUncaughtExceptionHandler(UncaughtExceptionHandler {
+            requireActivity().finish()
+
+            startActivity(Intent(requireActivity(), MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            })
+        })
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ExceptionThrowAndCatchFragment()
+    }
+}
