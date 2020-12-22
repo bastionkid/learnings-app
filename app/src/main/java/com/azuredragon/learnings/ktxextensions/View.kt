d4eb4b898d050a14392c542ae31f7cd66f89997a
bastionkid <akash.mercer@gmail.com>
2020-12-22 17:08:16 +0530

    1) View.kt : suspending View helper functions added

--- app/src/main/java/com/azuredragon/learnings/ktxextensions/View.kt
+++ app/src/main/java/com/azuredragon/learnings/ktxextensions/View.kt
@@ -1,15 +1,110 @@
 package com.azuredragon.learnings.ktxextensions
 
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 fun View.visible() {
-    this.visibility = View.VISIBLE
+    visibility = View.VISIBLE
 }
 
 fun View.invisible() {
-    this.visibility = View.INVISIBLE
+    visibility = View.INVISIBLE
 }
 
 fun View.gone() {
-    this.visibility = View.GONE
+    visibility = View.GONE
+}
+
+@ExperimentalCoroutinesApi
+suspend fun View.awaitTillNextLayout() = suspendCancellableCoroutine<Unit> { it ->
+    val listener = object: View.OnLayoutChangeListener {
+        override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
+            it.resume(Unit) {
+                removeOnLayoutChangeListener(this)
+            }
+        }
+    }
+
+    it.invokeOnCancellation {
+        removeOnLayoutChangeListener(listener)
+    }
+
+    addOnLayoutChangeListener(listener)
+}
+
+@ExperimentalCoroutinesApi
+suspend fun RecyclerView.Adapter<RecyclerView.ViewHolder>.getItemPosition(itemId: Long): Int {
+    //TODO Author:"Akash" Date:"22/12/20" What Needs to be Done:"Check if the existing data already has the itemId"
+    val position = 0
+
+    if (position >= 0) return position
+
+    return suspendCancellableCoroutine {
+        val adapterDataObserver = object: RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                for (itemPosition in positionStart..(positionStart + itemCount)) {
+                    if (itemId == getItemId(itemPosition)) {
+                        it.resume(itemPosition) {
+                            unregisterAdapterDataObserver(this)
+                        }
+
+                        return
+                    }
+                }
+            }
+        }
+
+        it.invokeOnCancellation {
+            unregisterAdapterDataObserver(adapterDataObserver)
+        }
+
+        registerAdapterDataObserver(adapterDataObserver)
+    }
+}
+
+@ExperimentalCoroutinesApi
+suspend fun View.awaitNextAnimationFrame() = suspendCancellableCoroutine<Unit> {
+    val runnable = object: Runnable {
+        override fun run() {
+            it.resume(Unit) {
+                removeCallbacks(this)
+            }
+        }
+    }
+
+    it.invokeOnCancellation {
+        removeCallbacks(runnable)
+    }
+
+    postOnAnimation(runnable)
+}
+
+@ExperimentalCoroutinesApi
+suspend fun RecyclerView.awaitTillScrollEnd() {
+    // If a smooth scroll has just been started, it won't actually start until the next
+    // animation frame, so we'll await that first
+    awaitNextAnimationFrame()
+
+    // Now we can check if we're actually idle. If so, return now
+    if (scrollState == RecyclerView.SCROLL_STATE_IDLE) return
+
+    suspendCancellableCoroutine<Unit> {
+        val scrollListener = object: RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    it.resume(Unit) {
+                        removeOnScrollListener(this)
+                    }
+                }
+            }
+        }
+
+        it.invokeOnCancellation {
+            removeOnScrollListener(scrollListener)
+        }
+
+        addOnScrollListener(scrollListener)
+    }
 }
