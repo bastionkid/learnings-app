0172ea1fa2ede3cce9cd89616f0f4a75ddece993
bastionkid <akash.mercer@gmail.com>
2021-03-04 15:11:03 +0530

    1) CustomSlider.kt : Hack added to always show the Slider tooltip

--- app/src/main/java/com/azuredragon/learnings/ui/custom/CustomSlider.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/custom/CustomSlider.kt
@@ -1,13 +1,22 @@
 package com.azuredragon.learnings.ui.custom
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import android.util.AttributeSet
 import com.azuredragon.learnings.ktxextensions.dpToPx
+import com.google.android.material.internal.DescendantOffsetUtils
+import com.google.android.material.internal.ViewUtils
 import com.google.android.material.slider.Slider
 import com.google.android.material.tooltip.TooltipDrawable
 
-class CustomSlider @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : Slider(context, attrs, defStyleAttr) {
+@SuppressLint("RestrictedApi")
+class CustomSlider @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : Slider(context, attrs, defStyleAttr) {
 
     init {
         try {
@@ -17,7 +26,46 @@ class CustomSlider @JvmOverloads constructor(context: Context, attrs: AttributeS
             labels?.forEach { label ->
                 label.setStroke(1.dpToPx(context).toFloat(), Color.parseColor("#CCCCCC"))
             }
+
+            addOnChangeListener { _, value, _ ->
+                try {
+                    labels?.firstOrNull()?.let { label ->
+                        label.text = "\u20B9 $value"
+
+                        val trackTopRef = Slider::class.java.superclass?.getDeclaredField("trackTop")
+                        trackTopRef?.isAccessible = true
+                        val trackTop = trackTopRef?.get(this) as? Int
+
+                        val labelPaddingRef = Slider::class.java.superclass?.getDeclaredField("labelPadding")
+                        labelPaddingRef?.isAccessible = true
+                        val labelPadding = labelPaddingRef?.get(this) as? Int
+
+                        val normalizeValueRef = Slider::class.java.superclass?.getDeclaredMethod("normalizeValue", Float::class.java)
+                        normalizeValueRef?.isAccessible = true
+
+                        val left = (trackSidePadding + (((normalizeValueRef?.invoke(this, value) as? Float) ?: 0f) * trackWidth).toInt() - label.intrinsicWidth / 2)
+                        val top = (trackTop ?: 0) - ((labelPadding ?: 0) + thumbRadius)
+                        label.setBounds(left, top - label.intrinsicHeight, left + label.intrinsicWidth, top)
+
+                        // Calculate the difference between the bounds of this view and the bounds of the root view to
+                        // correctly position this view in the overlay layer.
+
+                        // Calculate the difference between the bounds of this view and the bounds of the root view to
+                        // correctly position this view in the overlay layer.
+                        val rect = Rect(label.bounds)
+                        DescendantOffsetUtils.offsetDescendantRect(ViewUtils.getContentView(this)!!, this, rect)
+
+                        label.bounds = rect
+
+                        ViewUtils.getContentViewOverlay(this)?.add(label)
+                    }
+                } catch (e: NoSuchMethodException) {
+                    e.printStackTrace()
+                }
+            }
         } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        } catch (e: ClassNotFoundException) {
             e.printStackTrace()
         }
     }
