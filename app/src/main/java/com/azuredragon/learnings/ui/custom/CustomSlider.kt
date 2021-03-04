28124ec4d029cd6bd64bd95f6df43d3974103061
bastionkid <akash.mercer@gmail.com>
2021-03-04 13:34:55 +0530

    1) CustomSlider.kt : added to customize tooltip border styling

---
+++ app/src/main/java/com/azuredragon/learnings/ui/custom/CustomSlider.kt
@@ -0,0 +1,26 @@
+package com.azuredragon.learnings.ui.custom
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import com.azuredragon.learnings.ktxextensions.dpToPx
+import com.google.android.material.slider.Slider
+import com.google.android.material.tooltip.TooltipDrawable
+
+class CustomSlider @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : Slider(context, attrs, defStyleAttr) {
+
+    init {
+        try {
+            val labelsRef = Slider::class.java.superclass?.getDeclaredField("labels")
+            labelsRef?.isAccessible = true
+            val labels = labelsRef?.get(this) as? List<TooltipDrawable>
+            labels?.forEach { label ->
+                context?.let { context ->
+                    label.setStroke(1.dpToPx(context).toFloat(), Color.parseColor("#CCCCCC"))
+                }
+            }
+        } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        }
+    }
+}
--- app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
@@ -1,5 +1,7 @@
 package com.azuredragon.learnings.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.azuredragon.learnings.R
 import com.azuredragon.learnings.databinding.FragmentBlankBinding
@@ -8,6 +10,12 @@ import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
 class BlankFragment: Fragment(R.layout.fragment_blank) {
 
     private val blankBinding: FragmentBlankBinding by dataBindingsLazy()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        blankBinding.slider.setLabelFormatter { "\u20B9 $it" }
+    }
 
     companion object {
         @JvmStatic
