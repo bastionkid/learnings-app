8f92b5fc9f03a00de79493386fa9e7b4eb557104
bastionkid <akash.mercer@gmail.com>
2021-04-01 19:32:33 +0530

    1) ConstraintLayoutChildIssuesDetector.kt : Added

--- lintrules/src/main/java/com/azuredragon/lintrules/OurIssueRegistry.kt
+++ lintrules/src/main/java/com/azuredragon/lintrules/OurIssueRegistry.kt
@@ -3,19 +3,19 @@ package com.azuredragon.lintrules
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
+import com.azuredragon.lintrules.detectors.ConstraintLayoutChildIssuesDetector
 import com.azuredragon.lintrules.detectors.IncompleteTodoDetector
 import com.azuredragon.lintrules.detectors.InvalidVectorDrawableDetector
-import com.azuredragon.lintrules.detectors.TextViewIssuesDetector
 import com.azuredragon.lintrules.detectors.WrongContextGetColorUsageDetector
 
 @Suppress("UnstableApiUsage")
 class OurIssueRegistry: IssueRegistry() {
 
     override val issues: List<Issue> = listOf(
-        TextViewIssuesDetector.ISSUE,
         WrongContextGetColorUsageDetector.ISSUE,
         InvalidVectorDrawableDetector.ISSUE,
-        IncompleteTodoDetector.ISSUE
+        IncompleteTodoDetector.ISSUE,
+        ConstraintLayoutChildIssuesDetector.ISSUE
     )
 
     override val api: Int = CURRENT_API
---
+++ lintrules/src/main/java/com/azuredragon/lintrules/detectors/ConstraintLayoutChildIssuesDetector.kt
@@ -0,0 +1,48 @@
+package com.azuredragon.lintrules.detectors
+
+import com.android.SdkConstants
+import com.android.tools.lint.detector.api.*
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+
+@Suppress("UnstableApiUsage")
+class ConstraintLayoutChildIssuesDetector: LayoutDetector() {
+
+    //TODO Author:akashkhunt Date:01/04/21 What Needs to be Done:Refactor the hard-coded class name with SdkConstants.* alternative once available
+    override fun getApplicableElements(): Collection<String> {
+        return listOf("androidx.constraintlayout.widget.ConstraintLayout")
+    }
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        var currentNode: Node? = element.firstChild
+
+        while (currentNode != null) {
+            if (currentNode.textContent.contains(SdkConstants.VALUE_MATCH_PARENT)) {
+                context.report(
+                    issue = ISSUE,
+                    location = context.getLocation(currentNode),
+                    message = REPORT_MESSAGE,
+                    quickfixData = null
+                )
+            }
+
+            currentNode = currentNode.nextSibling
+        }
+    }
+
+    companion object {
+        private const val REPORT_MESSAGE = "It's recommended to use 0dp with constraints rather then using match_parent for layout_width & layout_height."
+
+        private val IMPLEMENTATION = Implementation(ConstraintLayoutChildIssuesDetector::class.java, Scope.RESOURCE_FILE_SCOPE)
+
+        val ISSUE = Issue.create(
+            id = "TextViewHardCodedAttributes",
+            briefDescription = "Use 0dp with constraints for layout_width and layout_height.",
+            explanation = "It's recommended to use 0dp with constraints rather then using match_parent for layout_width & layout_height.",
+            category = Category.CORRECTNESS,
+            priority = 5,
+            severity = Severity.ERROR,
+            implementation = IMPLEMENTATION
+        )
+    }
+}
