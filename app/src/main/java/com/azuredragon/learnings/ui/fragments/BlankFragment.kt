1ea2f86560c5716e3abf5db79742d0e7545e743e
bastionkid <akash.mercer@gmail.com>
2021-07-04 09:22:56 +0530

    1) avd_low_network_gradient.xml : Added 2) build.gradle : dependency versions bumped

--- app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
+++ app/src/main/java/com/azuredragon/learnings/ui/fragments/BlankFragment.kt
@@ -1,11 +1,13 @@
 package com.azuredragon.learnings.ui.fragments
 
-import android.app.Fragment
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.azuredragon.learnings.R
 import com.azuredragon.learnings.databinding.FragmentBlankBinding
 import com.azuredragon.learnings.ktxextensions.awaitTillNextLayout
+import com.azuredragon.learnings.ktxextensions.dataBindingsLazy
+import com.azuredragon.learnings.ktxextensions.viewLifeCycleScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class BlankFragment: Fragment(R.layout.fragment_blank) {
